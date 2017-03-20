dab7b8390855428857bedf12e1e1ea0c67c5a8c1
Jim Shaffer <jim@jimpdx.com>
2017-03-19 17:38:05 -0700

    midway through adding commets

--- AD Script.ps1
+++ AD Script.ps1
@@ -1,13 +1,25 @@
-﻿$MenuHeadText="Please select one of the followging items:"
+﻿#####################################################################
+#
+#  AD Utilities, created by Jim Shaffer, 3/7/17
+#  This software will run various AD utilities
+#  Software is provided as a pwoershell example, not be
+#  used in a production environment
+#
+#####################################################################
+
+
+# Menu items are strored in the array $MenuItems, add as needed
+
+$MenuHeadText="Please select one of the followging items:"
 $MenuItems= "",
 "Change static IP, subnet, Gateway on a remote server",  # item 1
 "Install AD services on a remote server",  # item 2
 "Promote a remote server to DC",  # item 3
 "Move or seize FSMO roles", #item 4
 "Exit" #item 5
-$MenuItemsCount = $MenuItems.length - 1
+$MenuItemsCount = $MenuItems.length - 1  # number of menu items
 $MenuSelectionText = "Selection (1-$MenuItemsCount)"
-$Lines = "`n`n`n"
+$Lines = "`n`n`n"  # used for spacing 
 
 
 Function Main{
@@ -19,8 +31,10 @@ Function MainMenu{
 Clear
 MainMenuDraw
 
+# prompt for input, keep asking if no valid answer (numeric < Menu items)
 While( ($Selection = Read-Host -Prompt $MenuSelectionText) -ne $MenuItemsCount){
     
+    # improve this in the future by putting the switch items in an array also
     switch($Selection){
     1 {Networking}
     2 {ADServices}
@@ -34,8 +48,9 @@ While( ($Selection = Read-Host -Prompt $MenuSelectionText) -ne $MenuItemsCount){
 }
 
 Function MainMenuDraw{
+# draw the title and main menu items only
 
-$ScreenWidth=$host.ui.RawUI.WindowSize.width - 4
+$ScreenWidth=$host.ui.RawUI.WindowSize.width - 4  # get screen witdth, draw banner within width
 $TitleMessage = "  JIM'S AD TOOLS SOFTWARE  "
 $TitleBanner = "#"*([int]($ScreenWidth/2) - [int]( $TitleMessage.Length / 2)) + $TitleMessage + "#"*([int]($ScreenWidth/2) - [int]( $TitleMessage.Length / 2))
 "$([int]($ScreenWidth/2) - [int]( $TitleMessage.Length / 2))"
@@ -46,13 +61,24 @@ $Lines
 Write-Host $TitleBanner "`n`n" -ForegroundColor Green
 $MenuHeadText + "`n"
 
-For ($x = 1; $x -le $MenuItemsCount ; $x++ )
+For ($x = 1; $x -le $MenuItemsCount ; $x++ )  # draw the menu items, improve by using fo each
     {
         "    " + $x  + " " + $MenuItems[$x] + "`n"
     }
 }
 
+#####################################################################
+#
+#  Start of utility functions
+#
+#####################################################################
+
+
 Function Networking{
+# this function will ask for new network information, IP, subnet mask, gateway
+# DNS server, will verify a good connection, then attampt to change the setting 
+# on the remote server
+# This function is currently NOT working and needs troubleshooting
 
 Clear
 
@@ -60,9 +86,11 @@ $Lines
 "Please enter the following parameters. You may enter [exit] to cancel at any time."
 "`n"
 
-$ErrorActionPreference = 'continue'
+$ErrorActionPreference = 'continue'  # this prevents ugly error messages on screen
 
 If ( ($Computer = Read-Host -Prompt "Name of server or current IP address") -eq "exit") { return }
+
+#test for connection before asking more details
 If (-not (Test-Connection $Computer -Count 1  )  ) {
     ProgressBar FAILED .3 Down
     $P = Read-Host -Prompt "`r`rFailed to connect.  Press enter to continue."
@@ -96,13 +124,12 @@ $NewDNS="$NewSub.$NewDNS"
 
 If ( ($P = Read-Host -Prompt "If these values look correct enter [y] to continue") -ne "y") { return }
 
-# remote over and change IP address
-# Development note - not working
-
+# remote over and change networking settings
 Invoke-Command -ComputerName $Computer -Credential $Cred -ScriptBlock{
     param ( $Computer, $NewIP, $NewSubne, $NewGateway, $NewDNS)
 
-    # below from https://blogs.technet.microsoft.com/heyscriptingguy/2012/02/28/use-powershell-to-configure-static-ip-and-dns-settings/
+    # following 4 lines are from
+    # https://blogs.technet.microsoft.com/heyscriptingguy/2012/02/28/use-powershell-to-configure-static-ip-and-dns-settings/
 
     $wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'" -computername $Computer
     $wmi.EnableStatic("$NewIP", "$NewSubnet")
@@ -110,12 +137,14 @@ Invoke-Command -ComputerName $Computer -Credential $Cred -ScriptBlock{
     $wmi.SetDNSServerSearchOrder("$NewDNS")
   }  -ArgumentList $Computer, $NewIP, $NewSubne, $NewGateway, $NewDNS
 
-ProgressBar SUCCESS 1 Up
+# need to add errror checking from previous lines, but for now assume success 
+ProgressBar SUCCESS 1 Up  
 $Lines
 $P = Read-Host -Prompt "Commands successefully proccessed. Press enter to continue." 
 
 "`nTesting new connection`n`n"
 
+# test connection to verify changes succeeded
 If (-not (Test-Connection $NewIP -Count 1  )  ) {
     ProgressBar FAILED .3 Down
     $P = Read-Host -Prompt "`r`rFailed to connect.  Press enter to continue."
@@ -129,6 +158,8 @@ $P = Read-Host -Prompt "Connection successeful. Press enter to continue."
 }
 
 Function ADServices{
+# This function will install Domain features on the remote server
+# need to test
 
 Clear
 $Lines
@@ -147,6 +178,7 @@ If ( ($Cred = Read-Host -Prompt "Enter username [default = lab\administrator]") 
 #Install the features
 Install-WindowsFeature AD-Domain-services -ComputerName $Computer -Credential $Cred -IncludeAllSubFeature
 
+# need to include error handling, assume success for now
 ProgressBar SUCCESS 1 Up
 $Lines
 $P = Read-Host -Prompt "Commands successeful. Press enter to continue."
@@ -154,6 +186,7 @@ $P = Read-Host -Prompt "Commands successeful. Press enter to continue."
 }
 
 Function Promote{
+# This function will pomete a remote server to a domain controller
 
 Clear
 $Lines
